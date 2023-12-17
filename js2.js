3c5d1c8b00bd3db4f38f02279589d09ae4640862
navadeepak <128801710+navadeepak@users.noreply.github.com>
2023-12-17 10:07:38 +0530

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,50 @@
+var nameinp=prompt("Enter Your Name: ");
+if (nameinp=="shreya"){
+    document.getElementById("ifindn").innerHTML=("HELLO, "+nameinp);
+}
+else if(nameinp=="Shreya"){
+    document.getElementById("ifindn").innerHTML=("HELLO, "+nameinp);
+}
+else if(nameinp=="SHREYA"){
+    document.getElementById("ifindn").innerHTML=("HELLO, "+nameinp);
+}
+else{
+    document.getElementById("ifindn").innerHTML=("HELLO, "+nameinp);
+}
+
+function abtu(){
+    if(nameinp=="Shreya" || nameinp=="shreya" || nameinp=="SHREYA")
+    {
+        document.getElementById("abtu").innerHTML=("You are a Sweet Person, and I Know About Your Self and I tell One Importent Thing That is Very Importent in My Point of You on Yours Birthday's gift😉✌️.");
+    }
+    else{
+        document.getElementById("abtu").innerHTML=("not to be update:)");
+    }
+}
+function howtrsu(){
+    if(nameinp=="Shreya" || nameinp=="shreya" || nameinp=="SHREYA")
+    {
+        document.getElementById("how").innerHTML=("Your are a one rare pice in the world, but i judge your problem is a you are searching a one good and supportive friend in the beautiful world.");
+    }
+    else{
+        document.getElementById("how").innerHTML=("not to be update:)");
+    }
+}
+function whichtypeu(){
+    if(nameinp=="Shreya" || nameinp=="shreya" || nameinp=="SHREYA")
+    {
+        document.getElementById("whichtypeu").innerHTML=(" you are one supportive person for me because however i stress level was increasing means i trying to connect with you because for a break the stress");
+    }
+    else{
+        document.getElementById("whichtypeu").innerHTML=("not to be update:)");
+    }
+}
+function sorry(){
+    if(nameinp=="Shreya" || nameinp=="shreya" || nameinp=="SHREYA")
+    {
+            document.getElementById("sorry").innerHTML="sorry";
+    }
+    else{
+        document.getElementById("sorry").innerHTML="have a nice day";
+    }
+}
